5d8057521a8a8d00b8c0ec120aaba807ba248a58
David Sanchez Valle <dsanva2004@gmail.com>
2026-05-20 08:13:56 -0500

    refactor(headings): promote Heading to h1 and HeadingSmall to h2

--- resources/js/components/Heading.vue
+++ resources/js/components/Heading.vue
@@ -9,7 +9,7 @@ defineProps<Props>();
 
 <template>
     <div class="mb-8 space-y-0.5">
-        <h2 class="type-heading text-foreground">{{ title }}</h2>
+        <h1 class="type-heading text-foreground">{{ title }}</h1>
         <p v-if="description" class="text-sm text-muted-foreground">
             {{ description }}
         </p>
