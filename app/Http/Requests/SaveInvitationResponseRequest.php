78ac5a0841a208c70e4a2b332269d866a9d92940
Lauana Oliveira Hubler <lauanahubler@gmail.com>
2026-09-20 18:06:29 -0500

    refactor: remove unnecessary method

--- app/Http/Requests/SaveInvitationResponseRequest.php
+++ app/Http/Requests/SaveInvitationResponseRequest.php
@@ -8,10 +8,6 @@ use Illuminate\Validation\Rule;
 
 class SaveInvitationResponseRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
 
     public function rules(): array
     {
